0d3a154d6836a51b7fa0b484512efbca725b291a
GhauriChishti <123356861+GhauriChishti@users.noreply.github.com>
2026-03-06 17:47:27 +0500

    Create all-products-widget.dart

---
+++ lib/widgets/all-products-widget.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AllProductsWidget extends StatelessWidget {
+  const AllProductsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
