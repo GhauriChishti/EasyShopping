bc9ca2a2994490ea7f0124e452f929c9b22950a9
GhauriChishti <123356861+GhauriChishti@users.noreply.github.com>
2026-03-06 17:42:30 +0500

    Create app-constant.dart

---
+++ lib/utils/app-constant.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static const String appMainName = 'Easy Shopping';
+  static const String appPoweredBy = 'Powered By Easy Shopping';
+
+  static const Color appMainColor = Color(0xFF0B5ED7);
+  static const Color appScendoryColor = Color(0xFF198754);
+  static const Color appTextColor = Colors.white;
+}
